31733fc5230187b4e25e9dfa83c48d26eff81934
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-06-10 17:34:45 +0200

    Open and close chrome with selenium

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import webbrowser
-webbrowser.open("http://www.youtube.com")
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+#os.system("start chrome") OPENS CHROME
+driver = webdriver.Chrome()
+#driver.get("http://www.python.org")
+#chrome_options = Options()
+#chrome_options.headless = True
+time.sleep(3)
+driver.close()

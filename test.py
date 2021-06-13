e62b9de1d7065e88a213dbfe86cdd566ee9d81c4
Lukas Hillbricht <codingwithhillbricht@gmail.com>
2021-06-13 23:53:51 +0200

    open chrome and hide window

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-#os.system("start chrome") OPENS CHROME
-driver = webdriver.Chrome()
-#driver.get("http://www.python.org")
-#chrome_options = Options()
+
+chrome_options = Options()
 #chrome_options.headless = True
+driver = webdriver.Chrome(options=chrome_options)
+#driver.get("http://www.python.org")
 time.sleep(3)
 driver.close()
